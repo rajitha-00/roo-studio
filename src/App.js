aba758e0085bf0fdf66eb9e9615d683620f49a7e
Rajitha Priyankara <rajithatw07102@gmail.com>
2023-02-23 00:36:57 +0530

    SCSS done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import Home from './views/Home'
+import Products from './views/Products'
+import Gallery from './views/Gallery'
+import AboutUs from './views/AboutUs'
+import ContactUs from './views/ContactUs'
 
 function App() {
   return (
     <>
-      
+    
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/products' element = {<Products/>}/>
+        <Route path='/gallery' element = {<Gallery/>}/>
+        <Route path='/aboutus' element = {<AboutUs/>}/>
+        <Route path='/contactus' element = {<ContactUs/>}/>
+      </Routes>
     </>
   );
 }
