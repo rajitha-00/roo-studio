def25780568865cd4f76f0e16b8376316f35ab70
Rajitha Priyankara <rajithatw07102@gmail.com>
2023-02-23 20:03:10 +0530

    home done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,51 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 import '../assets/scss/Home.css'
 const Card = (props) => {
   return (
     <>
-      <div className="card">
-        <div className="paper">
-            <div 
-            className="oneBgImg"
-            style={{
-            backgroundImage: `url(${props.photo_icon})`
-            }}>
+    <div className="wrapper">
 
-            </div>
-        </div>
+      <div className="innerWrapper">
+
+        <Grid  
+            container 
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+            spacing={1} 
+            rowSpacing={4} 
+            mt={5}
+            mb={5}
+            >
+            <Grid item lg={3} md={6} sm={12} xs={12}>
+              <div className="card">
+                <div className="paper" style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}>
+                </div>
+              </div>
+            </Grid>
+            <Grid item lg={3} md={6} sm={12} xs={12}>
+              <div className="card">
+                <div className="paper">
+                </div>
+              </div>
+            </Grid>
+            <Grid item lg={3} md={6} sm={12} xs={12}>
+              <div className="card">
+                <div className="paper " style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}>
+                </div>
+              </div>
+            </Grid>
+            <Grid item lg={3} md={6} sm={12} xs={12}>
+              <div className="card">
+                <div className="paper">
+                </div>
+              </div>
+            </Grid>
+        </Grid>
       </div>
+    </div>
+     
     </>
   )
 }
--- src/components/home/SectionHome.js
+++ src/components/home/SectionHome.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Card from '../Card'
+import '../../assets/scss/Home.css'
 
 const SectionHome = () => {
   return (
     <>
-      <Card
-       photo_icon={'https://miro.medium.com/v2/resize:fit:640/format:webp/1*TJcdctAUc66Nf74D9QSt8g.png'}
-      />
+      <Card />
     </>
   )
 }
