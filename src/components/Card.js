4660b9f81d5a232351a4a6e93eaaefc9a9044953
Rajitha Priyankara <rajithatw07102@gmail.com>
2023-02-23 20:20:16 +0530

    home 100% done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,10 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import '../assets/scss/Home.css'
-const Card = (props) => {
+import { motion } from "framer-motion";
+
+
+const Card = () => {
   return (
     <>
     <div className="wrapper">
@@ -20,26 +23,53 @@ const Card = (props) => {
             >
             <Grid item lg={3} md={6} sm={12} xs={12}>
               <div className="card">
-                <div className="paper" style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}>
-                </div>
+                <motion.div
+                className="paper" 
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.3,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}
+                style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}/>
+                
               </div>
             </Grid>
             <Grid item lg={3} md={6} sm={12} xs={12}>
               <div className="card">
-                <div className="paper">
-                </div>
+                <motion.div 
+                className="paper"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.5,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}/>
+               
               </div>
             </Grid>
             <Grid item lg={3} md={6} sm={12} xs={12}>
               <div className="card">
-                <div className="paper " style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}>
-                </div>
+                <motion.div 
+                className="paper" 
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.7,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}
+                 style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}/>
+              
               </div>
             </Grid>
             <Grid item lg={3} md={6} sm={12} xs={12}>
               <div className="card">
-                <div className="paper">
-                </div>
+                <motion.div 
+                className="paper"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.9,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}
+                />
+             
               </div>
             </Grid>
         </Grid>
