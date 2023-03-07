10439cc383e6c9fddd88bfaa1af7acf6406a7491
Rajitha Priyankara <rajithatw07102@gmail.com>
2023-03-07 17:29:08 +0530

    home done

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,12 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import '../assets/scss/Home.css'
 import { motion } from "framer-motion";
+import P1 from '../assets/images/home/2.jpeg'
+import P2 from '../assets/images/home/3.jpg'
+import P3 from '../assets/images/home/4.jpeg'
+import P4 from '../assets/images/home/5.jpg'
+import P5 from '../assets/images/home/6.jpg'
+import P6 from '../assets/images/home/7.jpg'
 
 
 const Card = () => {
@@ -21,7 +27,7 @@ const Card = () => {
             mt={5}
             mb={5}
             >
-            <Grid item lg={3} md={6} sm={12} xs={12}>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
               <div className="card">
                 <motion.div
                 className="paper" 
@@ -30,11 +36,11 @@ const Card = () => {
                 transition={{  delay: 0.3,
                   x: { duration: 1 },
                   default: { ease: "linear" } }}
-                style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}/>
+                  style={{ backgroundImage:`url(${P1})` }}/>
                 
               </div>
             </Grid>
-            <Grid item lg={3} md={6} sm={12} xs={12}>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
               <div className="card">
                 <motion.div 
                 className="paper"
@@ -42,11 +48,12 @@ const Card = () => {
                 animate={{ scale: 1.1 }}
                 transition={{  delay: 0.5,
                   x: { duration: 1 },
-                  default: { ease: "linear" } }}/>
-               
+                  default: { ease: "linear" } }}
+                  style={{ backgroundImage:`url(${P2})` }}/>
+                  
               </div>
             </Grid>
-            <Grid item lg={3} md={6} sm={12} xs={12}>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
               <div className="card">
                 <motion.div 
                 className="paper" 
@@ -55,11 +62,11 @@ const Card = () => {
                 transition={{  delay: 0.7,
                   x: { duration: 1 },
                   default: { ease: "linear" } }}
-                 style={{ backgroundImage:`url('https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}/>
+                  style={{ backgroundImage:`url(${P3})` }}/>
               
               </div>
             </Grid>
-            <Grid item lg={3} md={6} sm={12} xs={12}>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
               <div className="card">
                 <motion.div 
                 className="paper"
@@ -68,6 +75,35 @@ const Card = () => {
                 transition={{  delay: 0.9,
                   x: { duration: 1 },
                   default: { ease: "linear" } }}
+                  style={{ backgroundImage:`url(${P4})` }}
+                />
+             
+              </div>
+            </Grid>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
+              <div className="card">
+                <motion.div 
+                className="paper"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.9,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}
+                  style={{ backgroundImage:`url(${P5})` }}
+                />
+             
+              </div>
+            </Grid>
+            <Grid item lg={4} md={6} sm={12} xs={12}>
+              <div className="card">
+                <motion.div 
+                className="paper"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1.1 }}
+                transition={{  delay: 0.9,
+                  x: { duration: 1 },
+                  default: { ease: "linear" } }}
+                  style={{ backgroundImage:`url(${P6})` }}
                 />
              
               </div>
