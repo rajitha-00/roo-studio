d5686928abb4c3e0f08ad35a3cd202b6249aa9b6
Rajitha Priyankara <rajithatw07102@gmail.com>
2023-04-09 15:02:31 +0530

    footer creating

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+const Footer = () => {
+  return (
+    <>
+    
+      <WhatsAppIcon/>
+      <InstagramIcon/>
+      <FacebookIcon/>
+
+    </>
+  )
+}
+
+export default Footer
