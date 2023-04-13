1918e76e9b59e2a6c699700c7e9485313350115d
AnushkaBanda24 <anushkaprabath2017@gmail.com>
2023-04-13 09:39:04 +0530

    Contactus no color

--- src/components/contact/SectionContactus.jsx
+++ src/components/contact/SectionContactus.jsx
@@ -1,11 +1,15 @@
-import { Grid } from '@mui/material'
+import { Box, Button, Grid, TextField } from '@mui/material'
 import React from 'react'
+import '../../assets/scss/Contact.css'
+import SendIcon from '@mui/icons-material/Send';
 
 const SectionContactus = () => {
   return (
     <>
       <div className="wrapper">
-              <div className="innerWrapper-contact">
+        <div className="innerWrapper-contact">
+          
+          
                   <Grid
                     container 
                     direction="row"
@@ -16,9 +20,42 @@ const SectionContactus = () => {
                     mt={5}
                     mb={5}
                   >
-                      <Grid item lg={6} md={6} sm={12} xs={12}>
-                          <h1> Contact us  </h1>
-                          <h2> Name </h2>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
+              <div className='sendEmail'>
+              <div className='title'>
+                <h2> Contact <span>Us</span>  </h2>
+              </div>
+        <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              '& .MuiTextField-root': { width: '25ch' },
+            }}
+          >
+      
+                  <TextField label={'Your Name'} id="margin-none" className='textfield' />
+                  
+      
+                  <TextField label={'Email'} id="margin-dense" margin="dense" />
+                  
+      
+                  <TextField label={'Write a subject'} id="margin-normal" margin="normal" />
+
+                  <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+<br></br>
+                  <Button variant="contained" endIcon={<SendIcon />}>
+  Send
+</Button>
+                  
+
+                </Box>
+                </div>
                       </Grid>
                       
             </Grid >
