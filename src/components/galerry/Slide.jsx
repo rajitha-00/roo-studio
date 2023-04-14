a709789d364c48213764cd6690ea74531ada1b23
AnushkaBanda24 <anushkaprabath2017@gmail.com>
2023-04-14 21:47:28 +0530

    Gallery 20%

---
+++ src/components/galerry/Slide.jsx
@@ -0,0 +1,54 @@
+import { Box, Button, Grid } from '@mui/material'
+import '../../assets/scss/Slide.css'
+import React from 'react'
+import Collages from './Collages'
+
+const Slide = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="innerWrapper-contact">
+          
+          
+                  <Grid
+                    container 
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="center"
+                    spacing={1} 
+                    rowSpacing={4} 
+                    mt={5}
+                    mb={5}
+                  >
+            <Grid item lg={6} md={6} sm={12} xs={12}>
+              <div className='galleryBTN' > 
+              <div className='nameG' >
+                          <h2>Gallery</h2>
+                </div>
+                <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              '& .MuiTextField-root': { width: '25ch' },
+            }}
+          >
+                <Button variant="outlined">Collages</Button>
+                <Button variant="outlined">Weddings</Button>
+                <Button variant="outlined">Family</Button>
+
+                  </Box>
+                     </div>     
+ 
+                      </Grid>
+          </Grid >
+          
+        </div>
+      </div>
+      
+      <Collages/>
+          
+    </>
+  )
+}
+
+export default Slide
--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/galerry/Hero'
+import Slide from '../components/galerry/Slide'
 
 const Gallery = () => {
   return (
     <>
       <div className="container">
         <Hero />
+        < Slide />
         
       </div>
     </>
